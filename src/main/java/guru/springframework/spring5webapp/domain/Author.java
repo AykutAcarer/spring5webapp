c253266b1089fda66e16a8cacff3343082b1da77
Aykut Acarer <acareraykut@gmail.com>
2023-04-04 12:00:46 +0200

    updated to hibernate

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Author {
@@ -16,6 +18,8 @@ public class Author {
 	
 	private String firstName;
 	private String lastName;
+	
+	@ManyToMany(mappedBy = "authors")
 	private Set<Book> books;
 	
 	public Author() {
@@ -58,6 +62,29 @@ public class Author {
 
 	public void setBooks(Set<Book> books) {
 		this.books = books;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Author [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", books=" + books + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Author other = (Author) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.Objects;
 import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Book {
@@ -17,6 +19,10 @@ public class Book {
 	
 	private String title;
 	private String isbn;
+	
+	@ManyToMany
+	@JoinTable(name="author_book", joinColumns = @JoinColumn(name = "book_id"),
+				inverseJoinColumns = @JoinColumn(name = "author_id)"))
 	private Set<Author> authors;
 	
 	public Book() {	
@@ -61,6 +67,29 @@ public class Book {
 	public void setAuthors(Set<Author> authors) {
 		this.authors = authors;
 	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", isbn=" + isbn + ", authors=" + authors + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 	
 	
 	
